93f03d0fa18aa4af6c8987cd465da8da048b6e7e
benoitlgrwagon@gmail.com <benoitlgrwagon@gmail.com>
2025-02-18 11:56:08 +0000

    calculate the Operational Margin per Order

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -6,6 +6,6 @@ select
     ,SUM(purchase_cost) as purchase_cost
     ,SUM(margin) as margin
 from {{ ref('int_sales_margin') }}
--- where orders_id in (1002561,1002560,1002559) -used for comparing results
+-- where orders_id in (1002561,1002560,1002559) --used for comparing results
 group by orders_id, date_date
 order by orders_id
