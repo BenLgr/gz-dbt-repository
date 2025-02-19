f93f278cdea94062d019038ce653d2d6fe196725
B L <benoitlgrwagon@gmail.com>
2025-02-19 13:49:20 +0000

    Add jinja macro and package function call

--- models/green_weez/intermediate/int_campaigns.sql
+++ models/green_weez/intermediate/int_campaigns.sql
@@ -1,4 +1,4 @@
-select *
+/*select *
 from {{ ref('stg_raw__cpg_adwords') }}
 union all 
 select *
@@ -8,4 +8,22 @@ select *
 from {{ ref('stg_raw__cpg_criteo') }}
 union all 
 select *
-from {{ ref('stg_raw__cpg_facebook') }}
+from {{ ref('stg_raw__cpg_facebook') }}*/
+
+/* example with source 
+{{ dbt_utils.union_relations(
+    relations=[
+        source('raw', 'cpg_adwords'),
+        source('raw', 'cpg_bing'),
+        source('raw', 'cpg_criteo'),
+        source('raw', 'cpg_facebook')
+    ]
+) }} */
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__cpg_adwords'),
+        ref('stg_raw__cpg_bing'),
+        ref('stg_raw__cpg_criteo'),
+        ref('stg_raw__cpg_facebook')
+    ]
+) }} 
