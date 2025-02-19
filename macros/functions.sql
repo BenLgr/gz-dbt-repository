c468dde521da2fcd81ea108a9824d57d948e6566
B L <benoitlgrwagon@gmail.com>
2025-02-19 16:19:28 +0000

    add {} in macro

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,3 +1,3 @@
 {% macro margin_percent (revenue, purchase_cost) %}
-    ROUND( (revenue - purchase_cost) / revenue, 2)
+    ROUND( ({{revenue}} - {{purchase_cost}}) / {{revenue}}, 2)
 {% endmacro %}
